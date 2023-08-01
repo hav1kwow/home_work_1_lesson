7d61a119cacb63b5251d2cfe4ad4442348adf800
hav1k <anton.andrianov.94@mail.ru>
2023-08-01 21:16:35 +0300

    task1_tsk_2_3

---
+++ task1/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите первое число");
+
+Console.WriteLine("Введите второе число");
+
+String input = Console.ReadLine(); 
+
+int number = Convert.ToInt32(input);
+
+int result = number * number;
+
+String newString = ("Наш результат: " + result);
+
+Console.WriteLine(newString);
+
+Console.WriteLine("Наш результат: " + result);
+
---
+++ task2/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите число 1 ");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число 2 ");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+if (firstNumber == secondNumber * secondNumber)
+{
+    Console.Write("Да");
+}
+else
+{
+Console.Write("Нет");
+}
