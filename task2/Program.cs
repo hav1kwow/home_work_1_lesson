575cfa6d502fe5ae36fad41998263f5c4bf59db5
hav1k <anton.andrianov.94@mail.ru>
2023-08-01 22:23:03 +0300

    all_lesson_full

--- task2/Program.cs
+++ task2/Program.cs
@@ -1,4 +1,10 @@
-﻿Console.Write("Введите число 1 ");
+﻿// Задача №1. Напишите программу, которая на вход принимает два числа и проверяет, является ли первое число квадратом второго.
+// a = 25, b = 5 -> да 
+// a = 2, b = 10 -> нет 
+// a = 9, b = -3 -> да 
+// a = -3 b = 9 -> нет
+
+Console.Write("Введите число 1 ");
 int firstNumber = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите число 2 ");
 int secondNumber = Convert.ToInt32(Console.ReadLine());
--- task5/Program.cs
+++ task5/Program.cs
@@ -1,4 +1,10 @@
-﻿Console.WriteLine("Введите число");
+﻿
+// Напишите программу, которая на вход принимает одно число (N), а на выходе показывает все целые числа в промежутке от -N до N.
+// 4 -> "-4, -3, -2, -1, 0, 1, 2, 3, 4" 
+// 2 -> " -2, -1, 0, 1, 2"
+
+
+Console.WriteLine("Введите число");
 int firstNumbernumber = Convert.ToInt32(Console.ReadLine());
 int secondNumber = 0 - firstNumbernumber;
 int i = secondNumber;
