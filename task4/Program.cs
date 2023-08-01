4d2d54e9165c8e49b91a873aa7d91f377c703ea7
hav1k <anton.andrianov.94@mail.ru>
2023-08-01 22:20:22 +0300

    all_lesson

---
+++ task4/Program.cs
@@ -0,0 +1,13 @@
+﻿switch (number) {
+    case 1:
+    Console.WriteLine();
+    break; 
+
+    case 2:
+    Console.WriteLine();
+    break;
+
+    default:
+    Console.WriteLine();
+    break;
+}
---
+++ task5/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите число");
+int firstNumbernumber = Convert.ToInt32(Console.ReadLine());
+int secondNumber = 0 - firstNumbernumber;
+int i = secondNumber;
+while (i < firstNumbernumber + 1){
+    Console.Write(i+ "");
+    i++;
+}
