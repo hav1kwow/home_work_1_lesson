bfdd4d017a0db5e634fcea7fda912fa38b5cdab4
hav1k <anton.andrianov.94@mail.ru>
2023-08-03 21:49:16 +0300

    final_homework

--- Home work seminar 1-3/Program.cs
+++ Home work seminar 1-3/Program.cs
@@ -2,5 +2,10 @@
 // 5 -> 2, 4
 // 8 -> 2, 4, 6, 8
 
-Console.Write("Введите число 1 ");
+Console.WriteLine("Введите число 1 ");
 int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+    
+for (int i = 1; i <= firstNumber; i++)
+    if (i % 2 == 0)
+        Console.WriteLine(i);
