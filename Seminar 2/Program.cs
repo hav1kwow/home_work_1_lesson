fc5653866036ae047689ebfa90caa1107e7f79dd
hav1k <anton.andrianov.94@mail.ru>
2023-08-04 21:04:32 +0300

    second_lesson

---
+++ Seminar 2/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая выводит случайное число из отрезка 
+// [10, 99] и показывает наибольшую цифру числа.
+// Например:
+// 78 -> 8
+// 12-> 2
+// 85 -> 8
+// Random rand = new Random();
+// [10, 99] == 10 99
+// (10, 99) != 10 99
+//  int n = new Random().Next(10,100)
+
+Random rand = new Random();
+int number = rand.Next(10, 100);
+
+int a = number / 10;
+int b = number % 10;
+Console.WriteLine(number);
+
+if(a > b){
+    Console.WriteLine("Маскимальное число a =" + a);
+}
+else if(b > a){
+    Console.WriteLine("Максимальное {1} число b = {0}", b, a);
+}
+else {
+    Console.WriteLine($"Все числа равны {a} = {b}");
+}
---
+++ seminar 2-2/Program.cs
@@ -0,0 +1,23 @@
+﻿
+// Напишите программу, которая принимает на вход два числа и проверяет, 
+// является ли одно число квадратом другого.
+
+// 5, 25  ->  да
+// -4, 16  ->  да
+// 25, 5  ->  да
+// 8,9  ->  нет
+
+Console.Write("Введите число 1 ");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число 2 ");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+if (firstNumber == secondNumber * secondNumber || secondNumber == firstNumber * firstNumber)
+{
+    Console.Write("Да");
+}
+else
+{
+Console.Write("Нет");
+}
+
