09b3af9c9780151d142c5de7ae3a707e4ff62bed
hav1k <anton.andrianov.94@mail.ru>
2023-08-05 00:49:59 +0300

    home_work_2

---
+++ home work seminar 2-1/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.Write("Введите число ");
+int anyNumber = Convert.ToInt32(Console.ReadLine());
+string anyNumberr = Convert.ToString(anyNumber);
+
+if (anyNumberr.Length > 2){
+    Console.Write("anyNumber " + anyNumberr[2]);
+}
+else{
+    Console.Write("Третьей цифры нет");
+}
+
